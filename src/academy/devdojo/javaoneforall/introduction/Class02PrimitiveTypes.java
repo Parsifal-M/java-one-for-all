f5eb2a7527459e5a7076728df3444bd1820a5f2c
Peter Macdonald <13601053+Parsifal-M@users.noreply.github.com>
2022-03-03 17:18:43 +0100

    Initial commit

---
+++ src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -0,0 +1,16 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class02PrimitiveTypes {
+    public static void main(String[] args) {
+        //int, double, float, boolean, char, byte, short, long
+        int age = 10;
+        double doubleNum = 50.50;
+        float floatNum = 5000;
+        boolean trueBool = true;
+        boolean falseBool = false;
+        char myChar = 'P';
+        byte myByte = 40;
+        short myShort = 50;
+        System.out.println("Your age is "+age+ ", " +myChar);
+    }
+}
